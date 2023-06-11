26303dd3c7a2d18820402e192386f50a4d2c912f
yifan <yifanchen0914@gmail.com>
2023-06-11 23:16:29 +0800

    [file]: add docker compose

--- src/main/kotlin/com/yfckevin/springkotlinlearning/data/User.kt
+++ src/main/kotlin/com/yfckevin/springkotlinlearning/data/User.kt
@@ -4,17 +4,20 @@ import com.yfckevin.springkotlinlearning.data.dto.UserDto
 import javax.persistence.*
 
 @Entity
-@Table
+@Table(name = "member")
 data class User (
 
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         val id: Long?,
 
+        @Column
         var firstname: String,
 
+        @Column
         var lastname: String,
 
+        @Column
         var age: Int,
 
         ){
